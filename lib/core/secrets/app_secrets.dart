67fa43740f1b520ca252d9b93452a745a425c6fb
rahul-323 <143209137+rahul-323@users.noreply.github.com>
2024-09-19 16:00:48 +0530

    Update app_secrets.dart

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,5 +1,4 @@
 class Secrets {
-  static const supabaseUrl = "https://yhvrwyvbfkxkefnwauxf.supabase.co";
-  static const supabaseAnonKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlodnJ3eXZiZmt4a2VmbndhdXhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc0MDMzMDQsImV4cCI6MjAzMjk3OTMwNH0.hu5CWp352wmKOpmqpyhywDCuJXTS7VnVQwEKI1_IXlQ";
+  static const supabaseUrl = "your url";
+  static const supabaseAnonKey ="Your Key"
 }
